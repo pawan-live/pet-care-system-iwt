82aa20c86009b1db7587bde8f41e11bfd9c6e75a
pawan-live <pawan.senpura@gmail.com>
2022-05-06 16:01:19 +0530

    cleaned the code

--- js/styleguide.js
+++ js/styleguide.js
@@ -1,4 +1,19 @@
 let list = document.querySelectorAll(".class-snippet-copy");
+let classCont = document.querySelectorAll(".class-container");
+let tagCont = document.querySelectorAll(".tag-container");
+// let snippetIndex;
 
-// list.forEach
-// console.log("test");
+list.forEach((list, index) => {
+  list.addEventListener("click", () => {
+    let snippetClass = classCont[index].childNodes[1].innerHTML;
+    let snippetTag;
+    if (tagCont[index].childElementCount == 1) {
+      snippetTag = tagCont[index].children[0];
+    } else if (tagCont[index].childElementCount > 1) {
+      snippetTag = tagCont[index].children[1];
+    }
+    navigator.clipboard.writeText(snippetTag);
+    console.log(snippetClass, snippetTag, "Copied!");
+    // console.log(classCont[index]);
+  });
+});
