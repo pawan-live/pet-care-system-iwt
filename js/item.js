4538e5a79bb2c9607f9c36329f6d80200e476e6c
pawan-live <pawan.senpura@gmail.com>
2022-05-12 21:06:44 +0530

    added tabs below

---
+++ js/item.js
@@ -0,0 +1,21 @@
+function openTab(evt, tabName) {
+  var i, tabContent, tabButtons;
+
+  tabContent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
+  }
+
+  tabButtons = document.getElementsByClassName("tab-btn");
+  for (i = 0; i < tabButtons.length; i++) {
+    tabButtons[i].className = tabButtons[i].className.replace(
+      "active-tab",
+      "inactive-tab"
+    );
+  }
+
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += "active-btn";
+}
+
+// openTab();
